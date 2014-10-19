134ff38c6f7ac07c498b320bf5695c11f4b4ac90
Sergey Zakharchuk <rtridz@gmail.com>
2014-10-19 23:53:19 +0400

    ini

---
+++ README.md
@@ -0,0 +1,19 @@
+## Installation
+
+```sh
+pip install https://github.com/rtridz/sphinxapi/archive/master.zip
+```
+
+## Using
+
+```sh
+import sphinxapi
+```
+
+## Current version
+
+* Sphinx 2.1.7-release
+
+## Documentation
+
+[On the website](http://sphinxsearch.com/docs/)
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='sphinxapi',
+    version='2.1.7-release',
+    description='Sphinx search official client',
+    long_description=open('README.md').read(),
+    author='Sphinx Technologies Inc',
+    url='http://github.com/rtridz/sphinxapi',
+    license='GPL',
+    packages=('sphinxapi',),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
